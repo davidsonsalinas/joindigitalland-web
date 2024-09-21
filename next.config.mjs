b1b06dfb2c107f0656be79a08563f010fce8cd6d
davidsonsalinas <davidson.nando@hotmail.com>
2024-09-21 00:28:20 -0300

    build/eslint/remove-eslint-to-build-temp

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
       domains: ['assets.joindigitalland.com'],
     },
+    eslint: {
+        ignoreDuringBuilds: true, 
+      },
   };
   
   export default nextConfig;
